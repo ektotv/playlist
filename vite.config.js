22b3fa38a084de12cf99ece82197d97118f6f4ba
Liam Potter <liam@liampotter.co.uk>
2023-11-27 14:17:00 +0000

    Use DTS workaround to generate index.d.mts for ESM build

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,33 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import { resolve } from "node:path";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import { copyFileSync } from 'node:fs';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/main.ts"),
-      name: "playlist",
-      fileName: "playlist",
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'playlist',
+      fileName: 'playlist',
     },
   },
   plugins: [
     dts({
+      afterBuild: () => {
+        // To ensure the package is supported by all consumers, we must
+        // export types that are read as ESM. To do this, there must be
+        // duplicate types with the correct extension supplied in the
+        // package.json exports field.
+        copyFileSync('dist/index.d.ts', 'dist/index.d.mts');
+      },
       rollupTypes: true,
       insertTypesEntry: true,
     }),
   ],
   test: {
     coverage: {
-      provider: "v8",
+      provider: 'v8',
     },
   },
 });
