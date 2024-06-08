1e38b6e3c56a7601540128d550ef9067888d3379
Christian Benincasa <chrisbenincasa@gmail.com>
2024-06-08 07:25:26 -0400

    Export all types in generated declaration file
    Also replaces GenericObject type with built-in Record type

---
+++ .changeset/new-carrots-unite.md
@@ -0,0 +1,5 @@
+---
+"@iptv/playlist": patch
+---
+
+Export all types in generated declaration file
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { parseM3U } from "./parser.js";
 import { writeM3U } from "./writer.js";
-
+export * from "./types.js";
 export { parseM3U, writeM3U };
